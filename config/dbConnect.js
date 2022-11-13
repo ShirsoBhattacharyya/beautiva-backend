89f09b66031428cdf7e972913bdd562e9b00e808
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2022-11-13 21:03:00 +0530

    backend deployment

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 require("dotenv").config()
 
 const dbConnect=()=>{
-    return mongoose.connect(`${process.env.MONGODB_URL}`)
+    return mongoose.connect('mongodb+srv://beautiva:beautiva@beautiva-ecommerce.k9gresz.mongodb.net/beautiva-ecom')
     // return mongoose.connect(`mongodb://127.0.0.1:27017/beautiva-ecommerce`)
 }
 module.exports=dbConnect;
